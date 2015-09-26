ce9e4618246c47a59d2ea75e0ded42fb7fb0d201
Daniel Mathews <dannyl.mathews@gmail.com>
2015-09-26 10:32:48 -0700

    keys

--- Chipmunk/APIKeys2.swift
+++
@@ -1,9 +0,0 @@
-//
-//  APIKeys2.swift
-//  Chipmunk
-//
-//  Created by Daniel Mathews on 2015-09-26.
-//  Copyright © 2015 Daniel Mathews. All rights reserved.
-//
-
-import Foundation
---
+++ Chipmunk/KeysForAPIs.swift
@@ -0,0 +1,16 @@
+//
+//  KeysForAPIs.swift
+//  Chipmunk
+//
+//  Created by Daniel Mathews on 2015-09-26.
+//  Copyright © 2015 Daniel Mathews. All rights reserved.
+//
+
+import Foundation
+
+class APIKeys {
+    
+    class func parseApplicationID() -> String { return "" }
+    class func parseClientID() -> String { return "" }
+
+}
